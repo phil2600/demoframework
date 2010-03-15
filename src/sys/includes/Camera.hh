0ed859133d4ba838b9ab22aec0c581e1ed6bb043
Faure Philippe <faure.phil@gmail.com>
2010-03-15 01:16:47 +0100

    Code Refactoring
    Signed-off-by: Faure Philippe <faure.phil@gmail.com>

--- src/sys/includes/Camera.hh
+++ src/sys/includes/Camera.hh
@@ -6,48 +6,48 @@
 class CCamera
 {
 public:
-	CPoint m_vposition;
-	CPoint m_vlook;
-	CPoint m_vtarget;
+  CPoint m_vposition;
+  CPoint m_vlook;
+  CPoint m_vtarget;
 
-	CPoint m_vup;
-	CPoint m_vright;
+  CPoint m_vup;
+  CPoint m_vright;
 
-	float fov;
+  float fov;
 
-	float zNear,zFar;
-	float aspect;
+  float zNear,zFar;
+  float aspect;
 
-	CCamera();
+  CCamera();
 
-	void Place(CPoint position, CPoint targetPosition, CPoint upVector);
+  void Place(CPoint position, CPoint targetPosition, CPoint upVector);
 
-	void SetFov(float _fov);
-	float GetFov(void);
-	void SetFar(float nearz,float farz);
-	void SetAspect(float aspect);
-	void MoveForward(float distance);
-	void StrafeRight(float distance);
-	void MoveUpward(float distance);
-	void rotateX(float degrees);
-	void rotateY(float degrees);
-	void rotateZ(float degrees);
-	CPoint getUp();
-	CPoint getRight();
-	CPoint getTarget();
+  void SetFov(float _fov);
+  float GetFov(void);
+  void SetFar(float nearz,float farz);
+  void SetAspect(float aspect);
+  void MoveForward(float distance);
+  void StrafeRight(float distance);
+  void MoveUpward(float distance);
+  void rotateX(float degrees);
+  void rotateY(float degrees);
+  void rotateZ(float degrees);
+  CPoint getUp();
+  CPoint getRight();
+  CPoint getTarget();
 
-	void toOGL(void);
+  void toOGL(void);
 
-	bool inFrustrum(CPoint p);
+  bool inFrustrum(CPoint p);
 
 private:
 
-	void recalc(void);
-	unsigned int isReady;
+  void recalc(void);
+  unsigned int isReady;
 
-	void SetPos(float x,float y, float z);
-	void SetPos(CPoint _position);
-	void SetLook(float _x,float _y, float _z);
+  void SetPos(float x,float y, float z);
+  void SetPos(CPoint _position);
+  void SetLook(float _x,float _y, float _z);
 };
 
 #endif /* !CAMERA_HH_ */
--- src/sys/includes/graphic.hh
+++ src/sys/includes/graphic.hh
@@ -8,6 +8,60 @@
 # include "Algebre.hh"
 # include "Camera.hh"
 
+/* FlyCam */
+class FreeFlyCamera
+{
+public:
+  // FIXME
+  // Utiliser une camera directement dans l'environement
+  FreeFlyCamera(const CPoint & position = CPoint(0,0,0));
+
+  virtual void OnMouseMotion(const SDL_MouseMotionEvent & event);
+  virtual void OnMouseButton(const SDL_MouseButtonEvent & event);
+  virtual void OnKeyboard(const SDL_KeyboardEvent & event);
+
+  virtual void animate(Uint32 timestep);
+  virtual void setSpeed(double speed);
+  virtual void setSensivity(double sensivity);
+
+  virtual void setPosition(const CPoint & position);
+
+  virtual void look();
+
+  virtual ~FreeFlyCamera();
+
+  void moveForward(Uint32 distance, double speed);
+  void moveBackward(Uint32 distance, double speed);
+  void moveStrafeRight(Uint32 distance, double speed);
+  void moveStrafeLeft(Uint32 distance, double speed);
+
+  void rotate(float x, float y);
+protected:
+  double _speed;
+  double _sensivity;
+
+  //vertical motion stuffs
+  Uint32 _timeBeforeStoppingVerticalMotion;
+  bool _verticalMotionActive;
+  int _verticalMotionDirection;
+
+  typedef std::map<SDLKey,bool> KeyStates;
+  KeyStates _keystates;
+  typedef std::map<std::string,SDLKey> KeyConf;
+  KeyConf _keyconf;
+
+  CPoint _position;
+  CPoint _target;
+  CPoint _forward;
+  CPoint _left;
+  double _theta;
+  double _phi;
+
+  void VectorsFromAngles();
+  void recalc();
+};
+
+
 /* GraphicalEnvironment */
 class GraphEnv
 {
@@ -36,19 +90,22 @@ public:
   void     loadMatrix(CMatrix m);
   CMatrix  getProjectionMatrix();
   CMatrix  getModelViewMatrix();
-  CCamera* getActiveCamera();
-  void     setActiveCamera(CCamera *activeCamera);
+  FreeFlyCamera* getActiveCamera();
+  void     setActiveCamera(FreeFlyCamera *activeCamera);
 private:
   int  height_;
   int  width_;
   int  bpp_;
   bool fullscreen_;
-  std::string m_windowTitle;
 
-  CCamera m_dummyCamera;
-  CCamera *m_activeCamera;
+  // FIXME
+  // NOT used !
+  std::string windowTitle_;
 
+  FreeFlyCamera dummyCamera_;
+  FreeFlyCamera *activeCamera_;
 };
+
 
 int	init_GL(GraphEnv *graphical_env);
 
@@ -60,84 +117,6 @@ int	takeScreenshot(const char *filename);
 
 void	drawTVNoise(void);
 
-/* Vector3D */
-#define EPSILON  	1e-8
-#define ZERO  	 	EPSILON
 
-class Vector3D
-{
-public:
-  double X;
-  double Y;
-  double Z;
-
-  Vector3D();
-  Vector3D(double x,double y,double z);
-  Vector3D(const Vector3D & v);
-  Vector3D(const Vector3D & from,const Vector3D & to);
-
-  Vector3D & operator= (const Vector3D & v);
-
-  Vector3D & operator+= (const Vector3D & v);
-  Vector3D operator+ (const Vector3D & v) const;
-
-  Vector3D & operator-= (const Vector3D & v);
-  Vector3D operator- (const Vector3D & v) const;
-
-  Vector3D & operator*= (const double a);
-  Vector3D operator* (const double a)const;
-  friend Vector3D operator* (const double a,const Vector3D & v);
-
-  Vector3D & operator/= (const double a);
-  Vector3D operator/ (const double a)const;
-
-  Vector3D crossProduct(const Vector3D & v)const;
-  double length()const;
-  Vector3D & normalize();
-};
-
-
-/* FlyCam */
-class FreeFlyCamera
-{
-public:
-  FreeFlyCamera(const Vector3D & position = Vector3D(0,0,0));
-
-  virtual void OnMouseMotion(const SDL_MouseMotionEvent & event);
-  virtual void OnMouseButton(const SDL_MouseButtonEvent & event);
-  virtual void OnKeyboard(const SDL_KeyboardEvent & event);
-
-  virtual void animate(Uint32 timestep);
-  virtual void setSpeed(double speed);
-  virtual void setSensivity(double sensivity);
-
-  virtual void setPosition(const Vector3D & position);
-
-  virtual void look();
-
-  virtual ~FreeFlyCamera();
-protected:
-  double _speed;
-  double _sensivity;
-
-  //vertical motion stuffs
-  Uint32 _timeBeforeStoppingVerticalMotion;
-  bool _verticalMotionActive;
-  int _verticalMotionDirection;
-
-  typedef std::map<SDLKey,bool> KeyStates;
-  KeyStates _keystates;
-  typedef std::map<std::string,SDLKey> KeyConf;
-  KeyConf _keyconf;
-
-  Vector3D _position;
-  Vector3D _target;
-  Vector3D _forward;
-  Vector3D _left;
-  double _theta;
-  double _phi;
-
-  void VectorsFromAngles();
-};
 
 #endif /* !GRAPHIC_HH_ */
